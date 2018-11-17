4b71404fdb2a6e5a6923e52d982f44aefe5404f7
Rémi Lévesque <remi.levesque.3@ulaval.ca>
2018-11-17 17:06:35 -0500

    note parfaite pylint

--- parser_perso.py
+++ parser_perso.py
@@ -1,109 +1,105 @@
-import requests
+"""parser qui prend les coordonnées alphavantage pour renvoyer des valeurs de bourse"""
+
 import json
 import argparse
 import datetime
+import requests
 
 # ajout les coordonnées alphavantage
 
-url = 'https://www.alphavantage.co/query'
-function = 'TIME_SERIES_DAILY'
-apikey = '3PQRLNKE9VP5JH12'
-now = datetime.date.today()
+URL = 'https://www.alphavantage.co/query'
+FUNCTION = 'TIME_SERIES_DAILY'
+APIKEY = '3PQRLNKE9VP5JH12'
 
 
-# création du parser et ajout arguments à accepter (début, fin, valeur, help):
+# création du PARSER et ajout arguments à accepter (début, FIN, valeur, help):
 
-parser = argparse.ArgumentParser(
-    description="Extraction de valeurs historiques pour un symbole boursier",
-    conflict_handler='resolve')
-parser.add_argument('symbole', type=str, nargs='+',
-                    help="Nom du symbole boursier désiré")
-parser.add_argument('-d',  '--début', type=str,
-                    help="Date recherchée la plus ancienne (format: AAAA-MM-JJ)"
-                    )
-parser.add_argument('-f', '--fin', type=str,
+PARSER = argparse.ArgumentParser(description=
+                                 "Extraction de valeurs historique pour un symbole boursier",
+                                 conflict_handler='resolve')
+PARSER.add_argument('symbole', type=str, nargs='+', help="Nom du symbole boursier désiré")
+PARSER.add_argument('-d', '--début', type=str, metavar='DATE',
+                    help="Date recherchée la plus ancienne (format: AAAA-MM-JJ)")
+PARSER.add_argument('-f', '--FIN', type=str, metavar='DATE',
                     help="Date recherchée la plus récente (format: AAAA-MM-JJ)")
-parser.add_argument('-v', '--valeur', nargs=1, type=str, default='fermeture',
+PARSER.add_argument('-v', '--valeur', nargs=1, type=str, default='fermeture',
                     choices=['fermeture', 'ouverture', 'min', 'max', 'volume'],
-                    help="La valeur désirée(par défaut: fermeture)",
-                    metavar='DATE')
-args = parser.parse_args()
+                    help="La valeur désirée (par défaut: fermeture)")
+ARGS = PARSER.parse_args()
 
 # fonction pour convertir l'input de la date en datetime.date
 
 
 def conversion_date(date):
+    """convertir la date de l'input de l'utilisateur"""
     listedate = date.split('-')
-    année = int(listedate[0])
+    annee = int(listedate[0])
     mois = int(listedate[1])
     jour = int(listedate[2])
-    return(datetime.date(année, mois, jour))
+    return datetime.date(annee, mois, jour)
 
-# set valeur de début et fin en jours
+# set valeur de début et FIN en jours
 
 
-fin = conversion_date(args.fin)
-début = conversion_date(args.début)
-val = args.valeur[0]
+if ARGS.début is None and ARGS.FIN is None:
+    DEBUT = datetime.date.today()
+    FIN = DEBUT
+elif ARGS.FIN is None:
+    DEBUT = conversion_date(ARGS.début)
+    FIN = datetime.date.today()
+elif ARGS.début is None:
+    FIN = conversion_date(ARGS.FIN)
+    DEBUT = FIN
+else:
+    DEBUT = conversion_date(ARGS.début)
+    FIN = conversion_date(ARGS.FIN)
 
-# si la valeur de args.valeur = f, on veut avoir fermeture dans l'affichage
-if val == "f":
-    val = 'fermeture'
 
-# si on a pas d'input de début OU fin, on set à la date d'aujourd'hui
+VAL = ARGS.valeur[0]
 
-if args.début is None:
-    début = datetime.time.today()
-    fin = début
-if args.fin is None:
-    fin = datetime.time.today()
+"""si la valeur de ARGS.valeur = f, on veut avoir fermeture dans l'affichage"""
+
+if VAL == "f":
+    VAL = 'fermeture'
 
 # fonction du calcul boursier
 
 
 def calcul_boursier(symbole):
+    """fonction qui va chercher les données dans le dictionnaire alphavantage"""
     size = 'compact'
-    tdelta = fin - début
+    tdelta = FIN - DEBUT
 
-    # on set tdelta à la différence entre la fin et le début
-
-    if now - début > (100/5)*7:
+    if tdelta.days*5/7 > 100:
         size = 'full'
 
-    # plusieurs if statements pour les différents input de -v
-
-    if val == 'ouverture':
-        v = '1. open'
-    elif val == 'min':
-        v = '3. low'
-    elif val == 'max':
-        v = '2. high'
-    elif val == 'volume':
-        v = '5. volume'
+    if VAL == 'ouverture':
+        variable = '1. open'
+    elif VAL == 'min':
+        variable = '3. low'
+    elif VAL == 'max':
+        variable = '2. high'
+    elif VAL == 'volume':
+        variable = '5. volume'
     else:
-        v = '4. close'
-
-    # on fait une loop si jamais on a plus d'un symbole à évaluer
+        variable = '4. close'
 
     for symbol in symbole:
 
         params = {
-            'function': function,
+            'function': FUNCTION,
             'symbol': symbol,
-            'apikey': apikey,
+            'apikey': APIKEY,
             'outputsize': size,
         }
-        # connection serveur
-        response = requests.get(url=url, params=params)  # type: object
+
+        response = requests.get(url=URL, params=params)  # type: object
         response = json.loads(response.text)
-    # renvoi de message d'erreur si impossible de se connecter à Alphavantage
-        tuple1 = (
-            str(début), response['Time Series (Daily)'][str(début)][str(v)])
-        tuple2 = (str(fin), response['Time Series (Daily)'][str(fin)][str(v)])
-        print('{}({}, {}, {})'.format(symbol, val, str(début), str(fin)))
+        tuple1 = (str(DEBUT), response['Time Series (Daily)'][str(DEBUT)][str(variable)])
+        tuple2 = (str(FIN), response['Time Series (Daily)'][str(FIN)][str(variable)])
+        print('{}({}, {}, {})'.format(symbol, VAL, str(DEBUT), str(FIN)))
         print([tuple1, tuple2])
 
 
 if __name__ == '__main__':
-    # on éxécute la fonction calcul_boursier à l'appel du programme dans le term
-    calcul_boursier(args.symbole)
+    calcul_boursier(ARGS.symbole)
